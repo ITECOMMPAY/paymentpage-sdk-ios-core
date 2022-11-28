29e0825ee43cf8376e2d94dd008d350c21e9163f
Alexey Khrameev <a.khrameev@it.ecommpay.com>
2022-11-28 12:58:55 +0300

    version 0.4.5

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MsdkCore",
-            url: "https://github.com/ITECOMMPAY/paymentpage-sdk-ios-core/releases/download/0.5.3/MsdkCore.xcframework.zip",
-            checksum: "a3cb263da9f9a4cb4f17fb1d666ab7eb30930a664ee3c7a19707981a14bb5684"
+            url: "https://github.com/ITECOMMPAY/paymentpage-sdk-ios-core/releases/download/0.5.4/MsdkCore.xcframework.zip",
+            checksum: "bd21312c3e4332734449bc8704ab65d8c11d19849a1875bf7abe351ea910bf12"
         ),
     ]
 )
